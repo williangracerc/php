9adc39c2dac149136ce8fdee34f90e085a7d96a8
Willian Grace <williangracerc@gmail.com>
2018-09-01 05:24:47 -0300

    resolvi o conflito e fiz alteracoes

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
   	echo "Olá Mundo456!";
   
-  	echo "Olá Udemy!"
-=======
-  echo "Olá Mundo123!";
-  
-  echo "Olá HCODE!";
->>>>>>> 727acb96f1f38ea798ecbe1fa1243f82789f5edb
+  	echo "Olá Udemy e Hcode!"
+
 ?>
